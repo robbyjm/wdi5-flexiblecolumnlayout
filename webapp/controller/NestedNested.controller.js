039759094504bdcc512534f4abc9fa32a7efbd0e
Robby Manis <robbymanis@tutanota.com>
2023-03-24 00:50:01 -0400

    Add test cases
    Shows it is possible to target controls in nested views in the flexible column layout

---
+++ webapp/controller/NestedNested.controller.js
@@ -0,0 +1,15 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller",
+	"../model/formatter"
+], function(Controller, formatter) {
+	"use strict";
+
+	return Controller.extend("sap.ui.demo.basicTemplate.controller.Nested", {
+
+		formatter: formatter,
+
+		onInit: function () {
+
+		}
+	});
+});
